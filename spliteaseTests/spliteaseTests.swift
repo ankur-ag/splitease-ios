8c882dba0b71b0cdfc16efb7427df08419621a7b
Ankur Agarwal <outlaws01@gmail.com>
2026-01-08 09:04:19 +0800

    Initial Commit

---
+++ spliteaseTests/spliteaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  spliteaseTests.swift
+//  spliteaseTests
+//
+//  Created by Ankur on 1/8/26.
+//
+
+import Testing
+@testable import splitease
+
+struct spliteaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
